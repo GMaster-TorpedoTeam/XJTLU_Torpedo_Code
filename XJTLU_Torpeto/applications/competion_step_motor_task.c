4e24644eb33e3fc58e3434e36ff8df2114a33eaa
Mechanic <fanyushi11@126.com>
2022-06-23 14:44:32 +0800

    失控问题解决，系摩擦轮任务延时计数器没有归零

--- XJTLU_Torpeto/applications/competion_step_motor_task.c
+++ XJTLU_Torpeto/applications/competion_step_motor_task.c
@@ -19,36 +19,38 @@ void c_step_task(void const * argument)
 			{
 				c_step_motor_speed = 0;
 			}
-			else if (c_step_flag == 1 && push_high_state != 1)
+			else if (c_step_flag == 1)
 			{
 				c_step_motor_speed = 17000;
+				
+				if (push_high_state == 1)
+				{
+					c_step_flag = 3;
+					c_shoot_flag = 0;
+					end_flag = 1;
+				}
 				
 				delay_counter++;
 				if (delay_counter == delay_time)
 				{
+					delay_counter = 0;
 					c_step_flag = 3;
 					c_shoot_flag = 0;
 					finish_flag = 1;
 				}
 				
 			}
-			else if (c_step_flag == 2 && push_low_state != 0)
+			else if (c_step_flag == 2)
 			{
 				c_step_motor_speed = -17000;
+				
+				if ( push_low_state == 1)
+				{
+					c_step_flag = 0;
+					c_shoot_flag = 0;
+				}
 			}
 			
-			if (push_high_state == 1)
-			{
-				c_step_flag = 3;
-				c_shoot_flag = 0;
-				end_flag = 1;
-			}
-			
-			if ( push_low_state == 1)
-			{
-				c_step_flag = 0;
-				c_shoot_flag = 0;
-			}
 			
 			user_pid_Calc();
 			CAN_cmd_Torpedo(ShootMotor1.out, ShootMotor2.out, C_PushMotor_SPEED.out);
